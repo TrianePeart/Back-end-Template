0b2b28bab14a078734312cdeeb0a2bf0cd14b472
Triane Peart (She/They) <87388431+TrianePeart@users.noreply.github.com>
2023-02-07 12:36:30 -0500

    Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,12 +1,9 @@
-// File required for local and hosted apps
-
-// http://vitaly-t.github.io/pg-promise/module-pg-promise.html
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
 const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
   process.env;
-// https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
+
 const cn = DATABASE_URL
   ? {
       connectionString: DATABASE_URL,
@@ -22,9 +19,6 @@ const cn = DATABASE_URL
       user: PG_USER,
       password: PG_PASSWORD,
     };
-
-// alt from express docs
-// var db = pgp('postgres://username:password@host:port/database')
 
 const db = pgp(cn);
 
